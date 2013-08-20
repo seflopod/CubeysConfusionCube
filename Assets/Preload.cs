4820a5f169364ca400c1305c5ae572e26721658a
Peter Bartosch <bartoschp@gmail.com>
2013-08-19 22:22:41 -0500

    Web Player build?

---
+++ Assets/Preload.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Preload : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+#if UNITY_WEBPLAYER
+		if(Application.CanStreamedLevelBeLoaded(1))
+			Application.LoadLevel(1);
+#elif UNITY_STANDALONE
+		Application.LoadLevel(1);
+#endif
+	
+	}
+}
